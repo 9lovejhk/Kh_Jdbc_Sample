f54c403abb7fc566ef952039f054c097b6d97062
hrin <rrrin3255@gmail.com>
2022-09-08 12:19:19 +0900

    change

--- src/cafe/CafeMain.java
+++ src/cafe/CafeMain.java
@@ -4,7 +4,7 @@ import cafe.dao.MenuDAO;
 import cafe.vo.MenuVO;
 
 import java.util.*;
-
+///확인 확인
 // 보이시나요,,,, 네ㅔ....하위루..(o゜▽゜)o☆
 public class CafeMain {
     public static void main(String[] args) {
@@ -13,9 +13,6 @@ public class CafeMain {
         System.out.println("테스트테스틑222");
 
     }
-
-
-
     
     public static void masterSelect() {
         Scanner sc = new Scanner(System.in);
