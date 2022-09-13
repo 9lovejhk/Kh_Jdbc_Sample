ebf2036644af41641652b17c9741d05a0a9734b4
9lovejhk <9lovejhk@naver.com>
2022-09-13 14:56:46 +0900

    0913 first

--- src/cafe/CafeMain.java
+++ src/cafe/CafeMain.java
@@ -10,14 +10,14 @@ public class CafeMain {
         masterSelect();
 //        customerSelect();
     }
+
     public static void masterSelect() {
         Scanner sc = new Scanner(System.in);
         MenuDAO dao = new MenuDAO();
         while(true) {
             System.out.println("========== [관리자 계정] ==========");
-            System.out.println("▶▶ 실행할 작업을 선택하세요");
-            System.out.print("[1]메뉴판 보기\n[2]메뉴 추가하기\n[3]메뉴 삭제하기\n[4]메뉴 정보 수정하기\n[5]작업 종료 : ");
-            System.out.println();
+            System.out.print("[1]메뉴판 보기\n[2]메뉴 추가하기\n[3]메뉴 삭제하기\n[4]메뉴 정보 수정하기\n[5]일별 주문내역 조회\n[6]총 주문내역 조회\n[7]일별 메뉴당 매출 조회\n[8]작업 종료\n");
+            System.out.print("▶▶ 선택 : ");
             int sel = sc.nextInt();
             switch(sel) {
                 case 1:
@@ -34,6 +34,15 @@ public class CafeMain {
                     dao.menuUpdate();
                     break;
                 case 5 :
+                    dao.dailySales();
+                    break;
+                case 6 :
+                    dao.totalSales();
+                    break;
+                case 7 :
+                    dao.dailyMenuSales();
+                    break;
+                case 8 :
                     System.out.println("관리자 계정 로그아웃");
                     return;
             }
@@ -43,13 +52,11 @@ public class CafeMain {
     public static void customerSelect() {
         Scanner sc = new Scanner(System.in);
         MenuDAO dao = new MenuDAO();
-        System.out.println("========== [메뉴판] ==========");
-        System.out.println("(메뉴번호)   (메뉴명)   (메뉴금액)");
         List<MenuVO> list = dao.menuSelect();
         dao.menuSelectRst(list);
-        System.out.println("=============================");
         System.out.println();
-        System.out.print("[1]주문하기, [2]나가기 : ");
+        System.out.print("[1]주문하기\n[2]장바구니 보기\n[3]결제하기\n");
+        System.out.print("▶▶ 선택 : ");
         while(true) {
             int sel = sc.nextInt();
             switch(sel) {
@@ -58,13 +65,16 @@ public class CafeMain {
                     dao.customerOrder();
                     break;
                 case 2:
+                    dao.printBasket();
+                    break;
+                case 3:
                     System.out.println("다음에 또 오세요^^");
                     return;
             } // switch(sel)
 
             while (true) {
                 System.out.println();
-                System.out.print("[1]주문 추가하기 [2]장바구니 가기 [3]주문 종료 : ");
+                System.out.print("[1]주문 추가하기 [2]장바구니 가기 [3]결제하기 : ");
                 int add_sel = sc.nextInt();
                 switch(add_sel) {
                     case 1:
@@ -74,7 +84,7 @@ public class CafeMain {
                         dao.printBasket();
                         break;
                     case 3:
-                        System.out.println("주문을 종료 합니다");
+                        System.out.println("결제가 완료되었습니다.");
                         dao.endOrder();
                         return;
                 } // switch(add_sel)
--- src/cafe/dao/MenuDAO.java
+++ src/cafe/dao/MenuDAO.java
@@ -4,6 +4,8 @@ import cafe.util.Common;
 import cafe.vo.*;
 
 import java.sql.*;
+import java.sql.Date;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class MenuDAO {
@@ -49,6 +51,7 @@ public class MenuDAO {
             System.out.printf("%9d", e.getMenu_price());
             System.out.println();
         }
+        System.out.println("=============================");
         System.out.println();
     }
 
@@ -261,7 +264,6 @@ public class MenuDAO {
         } catch (Exception e) {
             e.printStackTrace();
         }
-        System.out.println();
         Common.close(rs);
         Common.close(stmt);
         Common.close(conn);
@@ -270,14 +272,137 @@ public class MenuDAO {
     public void endOrder() {
         MenuDAO dao = new MenuDAO();
 
-        String sql = "DELETE FROM CUSTOMER_ORDER";
+        String sql1 = "INSERT INTO ORDER_LIST(메뉴명, 메뉴금액, 사이즈, 사이즈금액, 옵션추가, 옵션추가금액, 수량, 합계) "
+                + "SELECT 메뉴명, 메뉴금액, CUSTOMER_ORDER.사이즈, 사이즈금액, 옵션추가, 옵션추가금액, 수량, ((메뉴금액+사이즈금액+옵션추가금액)*수량) 합계 "
+                + "FROM CUSTOMER_ORDER, MENU, OPTION_ADD, OPTION_SIZE "
+                + "WHERE CUSTOMER_ORDER.메뉴번호 = MENU.메뉴번호 "
+                + "AND CUSTOMER_ORDER.옵션번호 = OPTION_ADD.옵션번호 "
+                + "AND CUSTOMER_ORDER.사이즈 = OPTION_SIZE.사이즈";
+
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql1);
+            Common.close(rs);
+            Common.close(stmt);
+            Common.close(conn);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+//        String sql2 = "DELETE FROM CUSTOMER_ORDER";
+//
+//        try {
+//            conn = Common.getConnection();
+//            pstmt = conn.prepareStatement(sql2);
+//            pstmt.executeUpdate();
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        }
+    }
+
+    public void totalSales() {
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String t = "SELECT * FROM ORDER_LIST";
+            rs = stmt.executeQuery(t);
+
+            System.out.println();
+            int total = 0;
+            System.out.println("================== [총 주문내역] ==================");
+            System.out.println("  (주문일)  (메뉴명)  (메뉴금액)(사이즈)(사이즈금액) (옵션)   (추가금액)(수량)(합계)");
+            while (rs.next()) {
+                System.out.print(rs.getDate("주문일") );
+                System.out.printf("%10s ", rs.getString("메뉴명"));
+                System.out.printf("%6d  ", rs.getInt("메뉴금액"));
+                System.out.printf("%-7s", rs.getString("사이즈"));
+                System.out.printf("%-10s", rs.getInt("사이즈금액"));
+                System.out.printf("%-10s", rs.getString("옵션추가"));
+                System.out.printf("%-7s", rs.getInt("옵션추가금액"));
+                System.out.printf("%4d", rs.getInt("수량"));
+                System.out.printf("%9d", rs.getInt("합계"));
+                total += rs.getInt("합계");
+                System.out.println();
+            }
+            System.out.println("총 매출 금액은 " + total + "원 입니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
+    }
+
+    public void dailySales() {
+        System.out.print("조회할 날짜를 입력하세요(yy/mm/dd) : ");
+        String day = sc.next();
+
+        String sql = "SELECT * FROM ORDER_LIST WHERE TO_DATE(주문일, 'YY/MM/DD') = TO_DATE(?, 'YY/MM/DD') ";
 
         try {
             conn = Common.getConnection();
             pstmt = conn.prepareStatement(sql);
+            pstmt.setString(1, day);
             pstmt.executeUpdate();
+            rs = pstmt.getResultSet();
+
+            int total = 0;
+            System.out.println("================== [총 주문내역] ==================");
+            System.out.println("  (주문일)  (메뉴명)  (메뉴금액)(사이즈)(사이즈금액) (옵션)   (추가금액)(수량)(합계)");
+            while (rs.next()) {
+                System.out.print(rs.getDate("주문일") );
+                System.out.printf("%10s ", rs.getString("메뉴명"));
+                System.out.printf("%6d  ", rs.getInt("메뉴금액"));
+                System.out.printf("%-7s", rs.getString("사이즈"));
+                System.out.printf("%-10s", rs.getInt("사이즈금액"));
+                System.out.printf("%-10s", rs.getString("옵션추가"));
+                System.out.printf("%-7s", rs.getInt("옵션추가금액"));
+                System.out.printf("%4d", rs.getInt("수량"));
+                System.out.printf("%9d", rs.getInt("합계"));
+                total += rs.getInt("합계");
+                System.out.println();
+            }
+            System.out.println("총 매출 금액은 " + total + "원 입니다.");
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Common.close(rs);
+        Common.close(pstmt);
+        Common.close(conn);
+
+    }
+
+    public void dailyMenuSales() {
+        try {
+            conn = Common.getConnection();
+            stmt = conn.createStatement();
+            String sql = "SELECT NVL(TO_CHAR(주문일, 'YY/MM/DD'), '--------') 주문일, "
+                    + "NVL(메뉴명, '----TOTAL----') 메뉴명, SUM(수량) 수량, SUM(합계) 합계 "
+                    + "FROM ORDER_LIST "
+                    + "GROUP BY ROLLUP(TO_CHAR(주문일, 'YY/MM/DD'), 메뉴명)";
+            rs = stmt.executeQuery(sql);
+
+            System.out.println();
+            int total = 0;
+            System.out.println("========= [총 주문내역] =========");
+            System.out.println(" (주문일)     (메뉴명)   (수량)  (합계)");
+            while (rs.next()) {
+                System.out.printf("%-10s", rs.getString("주문일"));
+                System.out.printf("%13s", rs.getString("메뉴명"));
+                System.out.printf("%3d ", rs.getInt("수량"));
+                System.out.printf("%9d", rs.getInt("합계"));
+                total += rs.getInt("합계");
+                System.out.println();
+            }
+            System.out.println("총 매출 금액은 " + total + "원 입니다.");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println();
+        Common.close(rs);
+        Common.close(stmt);
+        Common.close(conn);
     }
 }
